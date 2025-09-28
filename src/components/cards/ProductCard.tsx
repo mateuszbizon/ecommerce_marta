57cdf0fce43c184a0c28363192ed09bffe30d0d2
Mateusz Bizoń <mat-biz@wp.pl>
2025-09-28 18:28:13 +0200

    adding product to basket

--- src/components/cards/ProductCard.tsx
+++ src/components/cards/ProductCard.tsx
@@ -1,14 +1,22 @@
+"use client"
+
 import { urlFor } from '@/sanity/lib/image'
 import { Product } from '@/sanity/types'
 import Image from 'next/image'
 import React from 'react'
 import { Button } from '../ui/button'
+import useBasketStore from '@/store/basket'
+import { Check } from 'lucide-react'
 
 type ProductCardProps = {
     product: Product
 }
 
 function ProductCard({ product }: ProductCardProps) {
+    const { addItem, getItemCount } = useBasketStore()
+    const productCount = getItemCount(product._id)
+    const isProductCountMoreZero = productCount > 0 ? true : false
+
   return (
     <div className='bg-background rounded-2xl border overflow-hidden py-5'>
         {product.image && (
@@ -19,8 +27,9 @@ function ProductCard({ product }: ProductCardProps) {
         <div className='p-5 pb-0 flex flex-col text-center gap-5'>
             <h3 className='bigger-text'>{product.name}</h3>
             <p className='heading3'>{product.price?.toFixed(2)} zł</p>
-            <Button variant={"destructive"}>
-                Wybieram
+            <Button variant={"destructive"} onClick={() => addItem(product)} disabled={isProductCountMoreZero}>
+                {isProductCountMoreZero ? "Dodano do koszyka" : "Wybieram"}
+                {isProductCountMoreZero && <Check className='size-5' />}
             </Button>
         </div>
     </div>
