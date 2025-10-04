c27e9c49cef1cceed714ba396e898aa8a7500d6e
Mateusz Bizoń <mat-biz@wp.pl>
2025-10-04 20:07:09 +0200

    added key for order status list

--- src/components/forms/OrderForm.tsx
+++ src/components/forms/OrderForm.tsx
@@ -22,8 +22,8 @@ function OrderForm({ orderStatus }: OrderFormProps) {
 
   return (
     <div className='flex gap-5 flex-wrap'>
-        {Object.values(ORDER_STATUS_ADMIN_MAP).map(item => (
-            <button className={`${selectedStatus === item.title ? "bg-gray-600" : "bg-transparent"} py-3 px-5 rounded-xl hover:bg-gray-600 cursor-pointer transition`} onClick={() => handleChangeStatus(item.title)}>
+        {Object.values(ORDER_STATUS_ADMIN_MAP).map((item, index) => (
+            <button key={`${item.title}_${index}`} className={`${selectedStatus === item.title ? "bg-gray-600" : "bg-transparent"} py-3 px-5 rounded-xl hover:bg-gray-600 cursor-pointer transition`} onClick={() => handleChangeStatus(item.title)}>
                 <span className={`px-3 py-1 rounded ${item.bg} font-semibold`}>
                     {item.title}
                 </span>
