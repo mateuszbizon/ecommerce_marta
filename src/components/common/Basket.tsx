6c6edc2792c8f70e782f19e174714b1ab651cedd
Mateusz Bizoń <mat-biz@wp.pl>
2025-10-05 20:15:01 +0200

    changed basket structure

--- src/components/common/Basket.tsx
+++ src/components/common/Basket.tsx
@@ -11,16 +11,18 @@ function Basket() {
     const allItemsQuantity = getAllItemsQuantity()
 
   return (
-    <Button size={"icon"} variant={"link"} className='text-foreground hover:text-primary relative' asChild>
-        <Link href={"/koszyk"}>
-            <ShoppingCart className='size-6' />
-            {allItemsQuantity > 0 && (
-                <div className='absolute -top-2 -right-2 flex justify-center items-center size-5 rounded-full bg-destructive text-xs '>
-                    <span>{allItemsQuantity}</span>
-                </div>
-            )}
-        </Link>
-    </Button>
+    <div className='relative'>
+        <Button size={"icon"} variant={"link"} className='text-foreground hover:text-primary' asChild>
+            <Link href={"/koszyk"}>
+                <ShoppingCart className='size-6' />
+            </Link>
+        </Button>
+        {allItemsQuantity > 0 && (
+            <div className='absolute -top-2 -right-2 flex justify-center items-center size-5 rounded-full bg-destructive text-xs '>
+                <span>{allItemsQuantity}</span>
+            </div>
+        )}
+    </div>
   )
 }
 
